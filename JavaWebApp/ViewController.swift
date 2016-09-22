f74db51e579b24ec2c785b2512e29cfd717139da
杨晓晨 <xiaochenyung@gmail.com>
2016-09-22 15:17:17 +0800

    获取json

--- JavaWebApp/ViewController.swift
+++ JavaWebApp/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
-
+import Alamofire
+import SVProgressHUD
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        Alamofire.request("http://192.168.11.184:8080/admin/users").responseJSON { (response) in
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
