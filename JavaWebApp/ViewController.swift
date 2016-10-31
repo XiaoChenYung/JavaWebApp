434c5b24cdbfbcf5ff463b6c1b8673ea890d5c91
杨晓晨 <xiaochenyung@gmail.com>
2016-10-31 17:16:08 +0800

    change

--- JavaWebApp/ViewController.swift
+++ JavaWebApp/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        Alamofire.request("http://192.168.11.184:8080/admin/users",method:.post,parameters:["nickname":"哥哥","firstName":"哇哈哈","lastName":"地雷","password":"圣诞节复活节阿斯"]).responseJSON { (response) in
+//        Alamofire.request("http://192.168.11.184:8080/admin/users",method:.post,parameters:["nickname":"哥哥","firstName":"哇哈哈","lastName":"地雷","password":"圣诞节复活节阿斯"]).responseJSON 
 
 
         
